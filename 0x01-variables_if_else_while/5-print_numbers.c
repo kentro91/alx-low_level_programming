00350f0bdf6c35498fc89818975fb86e92fe8611
kentrogenesis <kentrogenesis@gmail.com>
2022-10-16 09:06:04 -0700

    cd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+int ken;
+
+for (ken = 0; ken <= 20; ken++)
+{
+putchar(ken);
+}
+putchar('\n');
+return (0);
+}
