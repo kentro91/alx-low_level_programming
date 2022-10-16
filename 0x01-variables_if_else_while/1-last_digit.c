5fae4a76fe9506b612631aa9a6686b99a7e669fa
kentrogenesis <kentrogenesis@gmail.com>
2022-10-16 05:10:24 -0700

    cd

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,16 +7,15 @@
 * Return: Always 0
 */
 
-
 int main(void)
 {
 int n;
 int a;
- 
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 a = n % 10;
- 
+
 if (n > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, a);
