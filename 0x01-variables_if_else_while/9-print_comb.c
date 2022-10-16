61e93f34dcd7e900f57677e78741a9ce393abd08
kentrogenesis <kentrogenesis@gmail.com>
2022-10-16 11:53:23 -0700

    cd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,16 @@ int main(void)
 {
 int num;
 
-for (num = 0; num <= 10; num++)
+for (num = 0; num < 10; num++)
 {
-putchar("%d", num);
+putchar(num);
 
 if (num != 10)
 {
 putchar(',');
 putchar(' ');
 }
-}
 putchar('\n');
+}
 return (0);
 }
