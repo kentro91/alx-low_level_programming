9d622cbcef93baad13a93b4154056524aa06231e
kentrogenesis <kentrogenesis@gmail.com>
2022-10-15 21:23:14 -0700

    cd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -11,6 +12,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
 }
 printf("%d is positive\n", n);
