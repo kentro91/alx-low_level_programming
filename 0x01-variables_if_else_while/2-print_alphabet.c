cbffea81c1902519660d1c3519aa51abb55c2803
kentrogenesis <kentrogenesis@gmail.com>
2022-10-16 06:21:08 -0700

    cd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char ken;
+for(ken = a; ken <= z; ken++)
+{ 
+putchar(ken);
+}
+putchar("\n");
+return (0);  
+}
