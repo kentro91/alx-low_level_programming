832899ca66131e4eacd70de6b49b449d9b62d080
kentrogenesis <kentrogenesis@gmail.com>
2022-10-18 17:52:33 -0700

    Kentrogenesis

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
-*
-* program that prints _putchar, followed by a new line
-* Always: Return 0
+* main - prints _putchar, followed by a new line
+* Return: Always 0 (Success)
 */
 
 int main(void)
